98e9d37496f246c2caee4b881169f5b97bb052dd
MEELFFY <meelffy@gmail.com>
2025-06-02 02:49:59 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "Бот працює!"
+    return "Бот живий!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
